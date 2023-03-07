94d6de699138fb79f18ce7586fc3e37e4de9adaf
ekatwikz <ekatwikz@gmail.com>
2023-03-07 02:13:03 +0100

    created remaining tests and smacked em

--- StringCalcLib.Tests/StringCalc_Add_Should.cs
+++ StringCalcLib.Tests/StringCalc_Add_Should.cs
@@ -16,11 +16,76 @@ public class StringCalc_Add_Should
     }
 
     [Theory]
-    [InlineData("")]
-    [InlineData(null)]
-    public void Add_InputIsNullOrEmptyReturnsZero(string str)
-    {
-        int result = _stringCalc.Add(str);
-        Assert.True(result == 0, $"StringCalc::Add(\"{str}\") should return 0");
-    }
+    [InlineData("", 0)]
+    [InlineData(null, 0)]
+    public void Add_InputIsNullOrEmptyReturnsZero(string str, int expectedResult)
+        => Assert.True(_stringCalc.Add(str) == expectedResult, $"StringCalc::Add(\"{str}\") should return {expectedResult}");
+
+    [Theory]
+    [InlineData("0", 0)]
+    [InlineData("1", 1)]
+    [InlineData("3", 3)]
+    [InlineData("5", 5)]
+    public void Add_SingleValueReturnsUnchanged(string str, int expectedResult)
+        => Assert.True(_stringCalc.Add(str) == expectedResult, $"StringCalc::Add(\"{str}\") should return {expectedResult}");
+
+    [Theory]
+    [InlineData("1,1", 2)]
+    [InlineData("3,4", 7)]
+    [InlineData("9,9", 18)]
+    public void Add_CommaDelimitedReturnsSum(string str, int expectedResult)
+        => Assert.True(_stringCalc.Add(str) == expectedResult, $"StringCalc::Add(\"{str}\") should return {expectedResult}");
+
+    [Theory]
+    [InlineData("1\n1", 2)]
+    [InlineData("6\n9", 15)]
+    [InlineData("4\n7", 11)]
+    public void Add_NewlineDelimitedReturnsSum(string str, int expectedResult)
+        => Assert.True(_stringCalc.Add(str) == expectedResult, $"StringCalc::Add(\"{str}\") should return {expectedResult}");
+
+    [Theory]
+    [InlineData("1\n1,3", 5)]
+    [InlineData("2,6\n9", 17)]
+    [InlineData("4\n7\n1", 12)]
+    [InlineData("2,5,3", 10)]
+    public void Add_MixedDelimitersReturnsSum(string str, int expectedResult)
+        => Assert.True(_stringCalc.Add(str) == expectedResult, $"StringCalc::Add(\"{str}\") should return {expectedResult}");
+
+    [Theory]
+    [InlineData("-1")]
+    [InlineData("2,-2")]
+    [InlineData("3,-4")]
+    [InlineData("6\n-5,1")]
+    public void Add_NegativeCausesThrow(string str)
+        => Assert.Throws<ArgumentException>(() => _stringCalc.Add(str));
+
+    [Theory]
+    [InlineData("1000", 1000)]
+    [InlineData("1001,2", 2)]
+    [InlineData("2,1003\n4",6)]
+    [InlineData("3\n1337,7", 10)]
+    [InlineData("1\n420420,1", 2)]
+    public void Add_OverAThousandIgnored(string str, int expectedResult)
+        => Assert.True(_stringCalc.Add(str) == expectedResult, $"StringCalc::Add(\"{str}\") should return {expectedResult}");
+
+    [Theory]
+    [InlineData("//#\n1#3", 4)]
+    [InlineData("//?\n1?5,7", 13)]
+    [InlineData("//%2\n4%8", 14)]
+    public void Add_SingleCharDelimiterIsDefinable(string str, int expectedResult)
+        => Assert.True(_stringCalc.Add(str) == expectedResult, $"StringCalc::Add(\"{str}\") should return {expectedResult}");
+
+    [Theory]
+    [InlineData("[###]\n2###3", 5)]
+    [InlineData("[?#%]\n1?#%5,7", 13)]
+    [InlineData("[&*$]2\n4&*$8", 14)]
+    public void Add_MultiCharDelimiterIsDefinable(string str, int expectedResult)
+        => Assert.True(_stringCalc.Add(str) == expectedResult, $"StringCalc::Add(\"{str}\") should return {expectedResult}");
+
+    [Theory]
+    [InlineData("//#//!//?//*\n2*3?4#!#8", 17)]
+    [InlineData("[#!!][?@][),)]\n5?@3),)4#!!7", 19)]
+    [InlineData("[#!#]//?[*]1*3?7#!#8", 19)]
+    public void Add_MultipleDelimitersAreDefinable(string str, int expectedResult)
+        => Assert.True(_stringCalc.Add(str) == expectedResult, $"StringCalc::Add(\"{str}\") should return {expectedResult}");
 }
--- StringCalcLib/StringCalc.cs
+++ StringCalcLib/StringCalc.cs
@@ -1,13 +1,52 @@
 ﻿namespace StringCalcLib;
+
+using System.Text.RegularExpressions;
+
+public static class MeSpecialExtensions {
+    public static int SumAsIntButNoNegativeAndIgnoreOverAThousand(this int[] numbers) {
+        int sum = 0;
+
+        foreach (int num in numbers) {
+            if (num <= 1000) {
+                sum += num;
+            }
+
+            if (num < 0) {
+                throw new ArgumentException("Negative values not allowed!");
+            }
+        }
+
+        return sum;
+    }
+
+    public static (string, string[]) SeparateDelimsFromString(this string input, string[] defaultDelims) {
+        List<string> delims = new List<string>(defaultDelims);
+        foreach (Match match in Regex.Matches(input, @"\[(.*?)\]"))
+        {
+            delims.Add(match.Groups[1].Value);
+        }
+
+        foreach (Match match in Regex.Matches(input, @"\/\/(.)"))
+        {
+            delims.Add(match.Groups[1].Value);
+        }
+
+        return (Regex.Replace(input, @"\[.*?\]|\/\/.", ""), delims.ToArray());
+    }
+}
+
 public class StringCalc
-{
+{	
     public int Add(string input)
     {
-        if (String.IsNullOrEmpty(input))
+        if (input == null)
         {
             return 0;
         }
 
-        throw new NotImplementedException("Not quite done...");
+        string mainInput;
+        string[] delims;
+        (mainInput, delims) = input.SeparateDelimsFromString(new string[]{",", "\n"});
+        return Array.ConvertAll(mainInput.Split(delims, StringSplitOptions.RemoveEmptyEntries), int.Parse).SumAsIntButNoNegativeAndIgnoreOverAThousand();
     }
 }
